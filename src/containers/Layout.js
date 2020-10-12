48e2619c3794ba22401dd2858ade3bc637f66655
Igboanugwo Collins <munisco12@gmail.com>
2020-10-13 00:27:26 +0100

    implementing async css import

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -4,35 +4,60 @@ import React from 'react'
 import SubHeader from '../main/layout/SubHeader'
 import Footer from '../main/layout/Footer'
 
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import './assets/css/animate.min.css'
-// import './assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0'
-// import './assets/css/demo.css'
-// import './assets/css/pe-icon-7-stroke.css'
+//  const bootstrap = 'bootstrap/dist/css/bootstrap.min.css'
+// const animate = '../assets/css/animate.min.css'
+// const lightBootstrap = '../assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0'
+// const cssDemo = '../assets/css/demo.css'
+// const stroke = '../assets/css/pe-icon-7-stroke.css'
 
-function Layout(props) {
 
+// async function main(props) {
+    //     // const myModule = await import('./myModule.js')
+    
+    //     // const { export1, export2 } = await import('./myModule.js')
+    
+    //     const all = await Promise.all([
+        //         import('bootstrap/dist/css/bootstrap.min.css'),
+        //         import('../assets/css/animate.min.css'),
+        //         import('../assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0'),
+//     ])
+//     console.log('all promises', all)
+//     if(all){
+    //        return toRender = props.children
+//     }
+// }
+
+async function Layout(props) {
+    
+    let toRender;
 
     let isAdmin = props.isAdmin.includes('/admin')
-    let toRender;
     if(isAdmin){
-        console.log('Is the admin', props.isAdmin)
-        import('bootstrap/dist/css/bootstrap.min.css').then((Baz) => {
-            console.log('the import', Baz)
-        })
-        import('../assets/css/animate.min.css').then((Baz) => {
-            console.log('the import', Baz)
-        })
-        import('../assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0').then((Baz) => {
-            console.log('the import', Baz)
-        })
-        import('../assets/css/demo.css').then((Baz) => {
-            console.log('the import', Baz)
-        })
-        import('../assets/css/pe-icon-7-stroke.css').then((Baz) => {
-            console.log('the import', Baz)
-        })
+        // console.log('Is the admin', props.isAdmin)
+        // import(bootstrap).then((Baz) => {
+        //     console.log('the import', Baz)
+        // })
+        // import(animate).then((Baz) => {
+        //     console.log('the import', Baz)
+        // })
+        // import(lightBootstrap).then((Baz) => {
+        //     console.log('the import', Baz)
+        // })
+        // import(cssDemo).then((Baz) => {
+        //     console.log('the import', Baz)
+        // })
+        // import(stroke).then((Baz) => {
+        //     console.log('the import', Baz)
+        // })
+        //  await Promise.all([
+        //     import('bootstrap/dist/css/bootstrap.min.css'),
+        //     import('../assets/css/animate.min.css'),
+        //     import('../assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0'),
+        // ])
+        // console.log('all promises')
         toRender = props.children
+
+        console.log('the render', toRender)
     }
     else{
         console.log('Not the admin', props.isAdmin)
@@ -44,6 +69,8 @@ function Layout(props) {
                 <Footer />
             </div> </>
     }
+
+    console.log('the render', toRender)
 
     return (
         <>
