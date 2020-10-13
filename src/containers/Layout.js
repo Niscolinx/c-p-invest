be56f368cde48ddcf73f717bda575cdd423099ad
Igboanugwo Collins <munisco12@gmail.com>
2020-10-13 14:47:40 +0100

    working on the dashboard conditional css import

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 import SubHeader from '../main/layout/SubHeader'
 import Footer from '../main/layout/Footer'
+import { truncate } from 'fs';
 
 //  const bootstrap = 'bootstrap/dist/css/bootstrap.min.css'
 // const animate = '../assets/css/animate.min.css'
@@ -10,57 +11,54 @@ import Footer from '../main/layout/Footer'
 // const cssDemo = '../assets/css/demo.css'
 // const stroke = '../assets/css/pe-icon-7-stroke.css'
 
+function Layout(props) {
 
-// async function main(props) {
-    //     // const myModule = await import('./myModule.js')
-    
-    //     // const { export1, export2 } = await import('./myModule.js')
-    
-    //     const all = await Promise.all([
-        //         import('bootstrap/dist/css/bootstrap.min.css'),
-        //         import('../assets/css/animate.min.css'),
-        //         import('../assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0'),
-//     ])
-//     console.log('all promises', all)
-//     if(all){
-    //        return toRender = props.children
-//     }
-// }
+    const [state, setState] = useState({
+        content: ''
+    })
 
-async function Layout(props) {
-    
     let toRender;
 
     let isAdmin = props.isAdmin.includes('/admin')
     if(isAdmin){
-        // console.log('Is the admin', props.isAdmin)
-        // import(bootstrap).then((Baz) => {
-        //     console.log('the import', Baz)
-        // })
-        // import(animate).then((Baz) => {
-        //     console.log('the import', Baz)
-        // })
-        // import(lightBootstrap).then((Baz) => {
-        //     console.log('the import', Baz)
-        // })
-        // import(cssDemo).then((Baz) => {
-        //     console.log('the import', Baz)
-        // })
-        // import(stroke).then((Baz) => {
-        //     console.log('the import', Baz)
-        // })
-        //  await Promise.all([
-        //     import('bootstrap/dist/css/bootstrap.min.css'),
-        //     import('../assets/css/animate.min.css'),
-        //     import('../assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0'),
-        // ])
-        // console.log('all promises')
-        toRender = props.children
+        console.log('Is the admin', props.isAdmin)
 
-        console.log('the render', toRender)
+        localStorage.setItem('cssLoaded', false)
+        import('bootstrap/dist/css/bootstrap.min.css').then((Baz) => {
+            console.log('the import1', Baz)
+        })
+        import('../assets/css/animate.min.css').then((Baz) => {
+            console.log('the import2', Baz)
+        })
+        import('../assets/sass/light-bootstrap-dashboard-react.scss?v=1.3.0').then((Baz) => {
+            console.log('the import3', Baz)
+        })
+        import('../assets/css/demo.css').then((Baz) => {
+            console.log('the import4', Baz)
+        })
+       const all =  import('../assets/css/pe-icon-7-stroke.css').then(Baz => {
+            console.log('the import5', Baz)
+       
+        })
+        all.finally(result => {
+            console.log('resolved', result)
+            // localStorage.getItem('cssLoading')
+            localStorage.setItem('cssLoaded', true)
+            const isCssLoaded =  localStorage.getItem('cssLoaded')
+
+            if(isCssLoaded){
+                localStorage.setItem('loaded', true)
+            }
+
+        })
+       
+        console.log('local', localStorage.getItem('loaded'))
+        toRender = props.children
+        
     }
     else{
         console.log('Not the admin', props.isAdmin)
+        localStorage.clear('loaded', 'cssLoaded')
         toRender = <><div className='section-subHeader'>
                 <SubHeader />
             </div>
@@ -71,7 +69,7 @@ async function Layout(props) {
     }
 
     console.log('the render', toRender)
-
+    
     return (
         <>
             {toRender}
