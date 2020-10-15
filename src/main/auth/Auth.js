e2d2808ea0ffdaa8938f4329a3975e7af9cba0ff
Igboanugwo Collins <munisco12@gmail.com>
2020-10-15 10:55:55 +0100

    Added error message

--- src/main/auth/Auth.js
+++ src/main/auth/Auth.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import FlashMessage from 'react-flash-message'
 
 const Auth = (props) => {    
     let messageToDisplay = props.message ? Object.values(props.message) : null
@@ -9,7 +8,6 @@ const Auth = (props) => {
         width: messageToDisplay ? 'translateX(0)' : 'translateX(100%)',
     }
 
-    let alertMessage;
 
     const authMessage = ['auth__message']
 
@@ -17,39 +15,23 @@ const Auth = (props) => {
         authMessage.length > 1
             ? authMessage.pop() && authMessage.push('auth__message--error')
             : authMessage.push('auth__message--error')
-
-            alertMessage = (
-                <FlashMessage
-                    persistOnHover={true}
-                >
-                </FlashMessage>
-            )
     }
 
     if (messageToDisplay && props.message.success) {
         authMessage.length > 1
             ? authMessage.pop() && authMessage.push('auth__message--success')
             : authMessage.push('auth__message--success')
-
-            alertMessage = (
-                <FlashMessage
-                    className='auth__alert'
-                    persistOnHover={true}
-                >
-                    <p className={authMessage.join(' ')}>{messageToDisplay}</p>{' '}
-                </FlashMessage>
-            )
     }
-
-    console.log('the alert message', alertMessage)
 
 
     return (
         <section
             className={props.login ? 'auth-form auth-form__login' : 'auth-form'}
         >
-            {alertMessage}
-
+            <p className={authMessage.join(' ')} style={messageStyle}>
+                {messageToDisplay}
+            </p>
+            {props.children}
             {props.children}
         </section>
     )
