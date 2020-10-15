a7fc5fde6509cb968b805f0291bae92d7e84e361
Igboanugwo Collins <munisco12@gmail.com>
2020-10-15 10:54:39 +0100

    update

--- src/main/auth/Auth.js
+++ src/main/auth/Auth.js
@@ -4,19 +4,12 @@ import FlashMessage from 'react-flash-message'
 const Auth = (props) => {    
     let messageToDisplay = props.message ? Object.values(props.message) : null
     
-// const timeOut = () => {
-//     setTime({ text: 'Loading...' }, () => {
-//         setTimeout(() => {
-//             setTime({ text: 'view cart', showParagraph: true })
-//         }, 2000)
-//     })
-// }
-   // const timeOut = () => setTimeout(() => localStorage.setItem('clearError', 'true'), 2000)
-    
-   // console.log('the time', messageToDisplay)
+
     const messageStyle = {
-        display: messageToDisplay ? 'block' : 'none',
+        width: messageToDisplay ? 'translateX(0)' : 'translateX(100%)',
     }
+
+    let alertMessage;
 
     const authMessage = ['auth__message']
 
@@ -24,24 +17,38 @@ const Auth = (props) => {
         authMessage.length > 1
             ? authMessage.pop() && authMessage.push('auth__message--error')
             : authMessage.push('auth__message--error')
+
+            alertMessage = (
+                <FlashMessage
+                    persistOnHover={true}
+                >
+                </FlashMessage>
+            )
     }
 
     if (messageToDisplay && props.message.success) {
         authMessage.length > 1
             ? authMessage.pop() && authMessage.push('auth__message--success')
             : authMessage.push('auth__message--success')
+
+            alertMessage = (
+                <FlashMessage
+                    className='auth__alert'
+                    persistOnHover={true}
+                >
+                    <p className={authMessage.join(' ')}>{messageToDisplay}</p>{' '}
+                </FlashMessage>
+            )
     }
+
+    console.log('the alert message', alertMessage)
 
 
     return (
         <section
             className={props.login ? 'auth-form auth-form__login' : 'auth-form'}
         >
-            <FlashMessage duration={5000} persistOnHover={true}>
-                <p className={authMessage.join(' ')} style={messageStyle}>
-                    {messageToDisplay}
-                </p>{' '}
-            </FlashMessage>
+            {alertMessage}
 
             {props.children}
         </section>
--- src/react-app-env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react-scripts" />
-declare module 'react-bootstrap'
-declare module 'react-dom'
-declare module 'react-redux'
