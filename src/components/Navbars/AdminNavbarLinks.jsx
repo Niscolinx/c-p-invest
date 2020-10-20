7a02d4ad7b342bf9f04a67576b4fabedae22821b
Igboanugwo Collins <munisco12@gmail.com>
2020-10-20 17:22:35 +0100

    Building the fund account page

--- src/components/Navbars/AdminNavbarLinks.jsx
+++ src/components/Navbars/AdminNavbarLinks.jsx
@@ -1,6 +1,7 @@
 
 import React, { Component } from "react";
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 import { NavItem, Nav, NavDropdown, MenuItem } from "react-bootstrap";
 
 import * as orderAction from '../../store/actions/burgerIndex'
@@ -16,52 +17,52 @@ class AdminNavbarLinks extends Component {
       </div>
     );
     return (
-      <div>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            <i className="fa fa-dashboard" />
-            <p className="hidden-lg hidden-md">Dashboard</p>
-          </NavItem>
-          <NavDropdown
-            eventKey={2}
-            title={notification}
-            noCaret
-            id="basic-nav-dropdown"
-          >
-            <MenuItem eventKey={2.1}>Notification 1</MenuItem>
-            <MenuItem eventKey={2.2}>Notification 2</MenuItem>
-            <MenuItem eventKey={2.3}>Notification 3</MenuItem>
-            <MenuItem eventKey={2.4}>Notification 4</MenuItem>
-            <MenuItem eventKey={2.5}>Another notifications</MenuItem>
-          </NavDropdown>
-          <NavItem eventKey={3} href="#">
-            <i className="fa fa-search" />
-            <p className="hidden-lg hidden-md">Search</p>
-          </NavItem>
-        </Nav>
-        <Nav pullRight>
-          <NavItem eventKey={1} href="#">
-            Account
-          </NavItem>
-          <NavDropdown
-            eventKey={2}
-            title="Dropdown"
-            id="basic-nav-dropdown-right"
-          >
-            <MenuItem eventKey={2.1}>Action</MenuItem>
-            <MenuItem eventKey={2.2}>Another action</MenuItem>
-            <MenuItem eventKey={2.3}>Something</MenuItem>
-            <MenuItem eventKey={2.4}>Another action</MenuItem>
-            <MenuItem eventKey={2.5}>Something</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={2.5}>Separated link</MenuItem>
-          </NavDropdown>
-          <NavItem eventKey={7} onClick={this.props.onLogOut}>
-            Log out
-          </NavItem>
-        </Nav>
-      </div>
-    );
+        <div>
+            <Nav>
+                <NavItem eventKey={1} href='#'>
+                    <i className='fa fa-dashboard' />
+                    <p className='hidden-lg hidden-md'>Dashboard</p>
+                </NavItem>
+                <NavDropdown
+                    eventKey={2}
+                    title={notification}
+                    noCaret
+                    id='basic-nav-dropdown'
+                >
+                    <MenuItem eventKey={2.1}>Notification 1</MenuItem>
+                    <MenuItem eventKey={2.2}>Notification 2</MenuItem>
+                    <MenuItem eventKey={2.3}>Notification 3</MenuItem>
+                    <MenuItem eventKey={2.4}>Notification 4</MenuItem>
+                    <MenuItem eventKey={2.5}>Another notifications</MenuItem>
+                </NavDropdown>
+                <NavItem eventKey={3} href='#'>
+                    <i className='fa fa-search' />
+                    <p className='hidden-lg hidden-md'>Search</p>
+                </NavItem>
+            </Nav>
+            <Nav pullRight>
+                <NavItem eventKey={1}>
+                    <Link to='/admin/fund-account' className='adminNav__fundAccount'>Fund Account</Link>
+                </NavItem>
+                <NavDropdown
+                    eventKey={2}
+                    title='Dropdown'
+                    id='basic-nav-dropdown-right'
+                >
+                    <MenuItem eventKey={2.1}>Action</MenuItem>
+                    <MenuItem eventKey={2.2}>Another action</MenuItem>
+                    <MenuItem eventKey={2.3}>Something</MenuItem>
+                    <MenuItem eventKey={2.4}>Another action</MenuItem>
+                    <MenuItem eventKey={2.5}>Something</MenuItem>
+                    <MenuItem divider />
+                    <MenuItem eventKey={2.5}>Separated link</MenuItem>
+                </NavDropdown>
+                <NavItem eventKey={7} onClick={this.props.onLogOut}>
+                    Log out
+                </NavItem>
+            </Nav>
+        </div>
+    )
   }
 }
 
--- src/views/FundAccount.jsx
+++ src/views/FundAccount.jsx
@@ -1,87 +1,72 @@
 import React from 'react'
-import {FieldGroup, Checkbox, Radio, FormGroup, ControlLabel, FormControl, Button} from 'react-bootstrap'
+import {
+    Checkbox,
+    Radio,
+    FormGroup,
+    FormControl,
+    Button,
+    HelpBlock,
+} from 'react-bootstrap'
 
 function FundAccount() {
+    function FieldGroup({ id, label, help, ...props }) {
+        return (
+            <FormGroup controlId={id}>
+                <FormControl {...props} className='fundAccount__form--input' />
+                {help && <HelpBlock>Hello world</HelpBlock>}
+            </FormGroup>
+        )
+    }
+
     return (
-        <div>
-            <form>
+        <div className='fundAccount'>
+            <div className='fundAccount__balance'>
+                <h3 className='fundAccount__balance--text'>
+                    Available Balance: <span>$2321</span>
+                </h3>
+            </div>
+            <form className='fundAccount__form'>
                 <FieldGroup
                     id='formControlsText'
-                    type='text'
+                    type='number'
                     label='Text'
-                    placeholder='Enter text'
+                    placeholder='Enter amount - USD'
                 />
-                <FieldGroup
-                    id='formControlsEmail'
-                    type='email'
-                    label='Email address'
-                    placeholder='Enter email'
-                />
-                <FieldGroup
-                    id='formControlsPassword'
-                    label='Password'
-                    type='password'
-                />
-                <FieldGroup
-                    id='formControlsFile'
-                    type='file'
-                    label='File'
-                    help='Example block-level help text here.'
-                />
-
-                <Checkbox checked readOnly>
-                    Checkbox
-                </Checkbox>
-                <Radio checked readOnly>
-                    Radio
-                </Radio>
-
-                <FormGroup>
-                    <Checkbox inline>1</Checkbox> <Checkbox inline>2</Checkbox>{' '}
-                    <Checkbox inline>3</Checkbox>
-                </FormGroup>
-                <FormGroup>
-                    <Radio name='radioGroup' inline>
-                        1
-                    </Radio>{' '}
-                    <Radio name='radioGroup' inline>
-                        2
-                    </Radio>{' '}
-                    <Radio name='radioGroup' inline>
-                        3
-                    </Radio>
-                </FormGroup>
 
                 <FormGroup controlId='formControlsSelect'>
-                    <ControlLabel>Select</ControlLabel>
                     <FormControl componentClass='select' placeholder='select'>
-                        <option value='select'>select</option>
-                        <option value='other'>...</option>
+                        <option value='Bitcoin'>Bitcoin</option>
+                        <option value='Ethereum'>Ethereum</option>
                     </FormControl>
                 </FormGroup>
-                <FormGroup controlId='formControlsSelectMultiple'>
-                    <ControlLabel>Multiple select</ControlLabel>
-                    <FormControl componentClass='select' multiple>
-                        <option value='select'>select (multiple)</option>
-                        <option value='other'>...</option>
-                    </FormControl>
-                </FormGroup>
-
-                <FormGroup controlId='formControlsTextarea'>
-                    <ControlLabel>Textarea</ControlLabel>
-                    <FormControl
-                        componentClass='textarea'
-                        placeholder='textarea'
-                    />
-                </FormGroup>
-
-                <FormGroup>
-                    <ControlLabel>Static text</ControlLabel>
-                    <FormControl.Static>email@example.com</FormControl.Static>
-                </FormGroup>
-
-                <Button type='submit'>Submit</Button>
             </form>
+
+            <div className='fundAccount__upload'>
+                <form>
+                    <FieldGroup
+                        id='formControlsFile'
+                        type='file'
+                        label='File'
+                        help='Example block-level help text here.'
+                    />
+
+                    <Checkbox checked readOnly>
+                        Checkbox
+                    </Checkbox>
+                    <Radio checked readOnly>
+                        Radio
+                    </Radio>
+
+                    <FormGroup className='fundAccount__form--contact'>
+                        <FormControl.Static>
+                            Contact management at
+                            support@coinperfectinvestment.com for other payment
+                            methods
+                        </FormControl.Static>
+                    </FormGroup>
+                    <Button type='submit'>Submit</Button>
+                </form>
+            </div>
         </div>
     )
 }
