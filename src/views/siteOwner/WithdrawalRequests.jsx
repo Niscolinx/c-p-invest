50bbf0e0a4c04adc47b62247c74a7d0939b50b85
Igboanugwo Collins <munisco12@gmail.com>
2020-10-31 07:34:08 +0100

    Added the dates selection

--- src/views/siteOwner/WithdrawalRequests.jsx
+++ src/views/siteOwner/WithdrawalRequests.jsx
@@ -1,13 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function WithdrawalRequests(props) {
+    const [amount, setAmount] = useState('')
+    const [currency, setCurrency] = useState('Bitcoin')
+    const [file, setFile] = useState('')
+    const [imagePreview, setImagePreview] = useState(null)
+    const [planDetails, setPlanDetails] = useState({})
+
+    const withdrawalFromDateChange = (e) => {
+        console.log('the from date', e.target)
+        // setAmount(e.target.value)
+    }
+    const withdrawalToDateChange = (e) => {
+        console.log('the from date', e.target)
+        // setAmount(e.target.value)
+    }
+    const handleSelectChange = (e) => {
+        setCurrency(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = {
+            amount,
+            currency,
+            file,
+        }
+    }
     return (
-        <div>
-            
+        <div className='withdrawalRequest'>
+            <div className='withdrawalRequest-left'>
+                <form
+                    className='withdrawalRequest-left__form'
+                    onSubmit={handleSubmit}
+                >
+                    <input
+                        type='date'
+                        className='withdrawalRequest-left__form--input'
+                        name='withdrawalFromDate'
+                        id='withdrawalFromDate'
+                        onChange={withdrawalFromDateChange}
+                        // value={amount}
+                    />
+                    <input
+                        type='date'
+                        className='withdrawalRequest-left__form--input'
+                        name='withdrawalToDate'
+                        id='withdrawalToDate'
+                        onChange={withdrawalToDateChange}
+                        // value={amount}
+                    />
+
+                    <select
+                        name='select'
+                        id='select'
+                        onChange={handleSelectChange}
+                        value={currency}
+                        className='withdrawalRequest-left__form--select'
+                    >
+                        <option value='Bitcoin'>All e-currencies</option>
+                        <option value='Bitcoin'>Bitcoin</option>
+                        <option value='Ethereum'>Ethereum</option>
+                    </select>
+
+                    <div className='withdrawalRequest-left__form--btn'>
+                        {/* <button
+                            type='submit'
+                            className='withdrawalRequest-left__form--btn-item'
+                        >
+                            Submit
+                        </button> */}
+
+                        <button type='submit' className='button'>
+                            Proceed
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div className='withdrawalRequest-right'></div>
+            <div className='withdrawalRequest-details'></div>
         </div>
     )
 }
 
-
 export default WithdrawalRequests
-
