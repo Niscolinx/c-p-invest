c65087d8bbaff98fa75f723a7dab67bf8d91468b
Igboanugwo Collins <munisco12@gmail.com>
2020-10-31 08:22:23 +0100

    Added the table and inputs to the withdrawal request

--- src/views/siteOwner/WithdrawalRequests.jsx
+++ src/views/siteOwner/WithdrawalRequests.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react'
+import { Grid, Row, Col, Table } from 'react-bootstrap'
+
+import Card from '../../components/Card/Card'
+import { thWithdrawalArray, tdWithdrawalArray } from '../../variables/Variables'
 
 function WithdrawalRequests(props) {
     const [withdrawalFromDate, setWithdrawalFromDate] = useState('')
     const [withdrawalToDate, setWithdrawalToDate] = useState('')
+    const [fromAmount, setFromAmount] = useState('')
+    const [toAmount, setToAmount] = useState('')
+    const [perPage, setPerPage] = useState('10')
     const [currency, setCurrency] = useState('All Currencies')
 
     const withdrawalFromDateChange = (e) => {
@@ -13,6 +20,15 @@ function WithdrawalRequests(props) {
     }
     const handleSelectChange = (e) => {
         setCurrency(e.target.value)
+    }
+    const handleFromAmount = (e) => {
+        setFromAmount(e.target.value)
+    }
+    const handleToAmount = (e) => {
+        setToAmount(e.target.value)
+    }
+    const handlePerPage = (e) => {
+        setPerPage(e.target.value)
     }
 
     const handleSubmit = (e) => {
@@ -46,37 +62,51 @@ function WithdrawalRequests(props) {
                         value={withdrawalToDate}
                     />
 
-                    <select
-                        name='select'
-                        id='select'
-                        onChange={handleSelectChange}
-                        value={currency}
-                        className='withdrawalRequest-left__form--select'
-                    >
-                        <option value='All Currencies'>All Currencies</option>
-                        <option value='Bitcoin'>Bitcoin</option>
-                        <option value='Ethereum'>Ethereum</option>
-                    </select>
+                    <input
+                        type='input'
+                        className='withdrawalRequest__form--input'
+                        name='perPage'
+                        placeholder='per page'
+                        id='perPage'
+                        onChange={handlePerPage}
+                        value={perPage}
+                    />
+
                     <div className='withdrawalRequest-right'>
-                        <label className='withdrawalRequest-right__label'>Amount</label>
+                        <label className='withdrawalRequest__label'>
+                            Amount
+                        </label>
                         <input
-                            type='input'
+                            type='number'
                             className='withdrawalRequest__form--input'
-                            name='withdrawalToDate'
-                            placeholder='From'
-                            id='withdrawalToDate'
-                            onChange={withdrawalToDateChange}
-                            value={withdrawalToDate}
+                            name='fromAmount'
+                            placeholder='From 0.00'
+                            id='fromAmount'
+                            onChange={handleFromAmount}
+                            value={fromAmount}
                         />
                         <input
-                            type='input'
+                            type='number'
                             className='withdrawalRequest__form--input'
-                            name='withdrawalToDate'
-                            placeholder='To'
-                            id='withdrawalToDate'
-                            onChange={withdrawalToDateChange}
-                            value={withdrawalToDate}
+                            name='toAmount'
+                            placeholder='To 0.00'
+                            id='toAmount'
+                            onChange={handleToAmount}
+                            value={toAmount}
                         />
+                        <select
+                            name='select'
+                            id='select'
+                            onChange={handleSelectChange}
+                            value={currency}
+                            className='withdrawalRequest__form--select'
+                        >
+                            <option value='All Currencies'>
+                                All Currencies
+                            </option>
+                            <option value='Bitcoin'>Bitcoin</option>
+                            <option value='Ethereum'>Ethereum</option>
+                        </select>
                     </div>
 
                     <div className='withdrawalRequest__form--btn'>
@@ -86,7 +116,68 @@ function WithdrawalRequests(props) {
                     </div>
                 </form>
             </div>
-            <div className='withdrawalRequest-details'></div>
+            <div className='withdrawalRequest-details'>
+                <div className='content'>
+                    <Grid fluid>
+                        <Row>
+                            <Col md={12}>
+                                <Card
+                                    plain
+                                    title='Your investment'
+                                    category='History'
+                                    ctTableFullWidth
+                                    ctTableResponsive
+                                    content={
+                                        <Table>
+                                            <thead>
+                                                <tr>
+                                                    {thWithdrawalArray.map(
+                                                        (prop, key) => {
+                                                            return (
+                                                                <th key={key}>
+                                                                    {prop}
+                                                                </th>
+                                                            )
+                                                        }
+                                                    )}
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                {tdWithdrawalArray.map(
+                                                    (prop, key) => {
+                                                        return (
+                                                            <tr key={key}>
+                                                                {prop.map(
+                                                                    (
+                                                                        prop,
+                                                                        key
+                                                                    ) => {
+                                                                        return (
+                                                                            <td
+                                                                                key={
+                                                                                    key
+                                                                                }
+                                                                            >
+                                                                                {
+                                                                                    prop
+                                                                                }
+                                                                            </td>
+                                                                        )
+                                                                    }
+                                                                )}
+                                                            </tr>
+                                                        )
+                                                    }
+                                                )}
+                                            </tbody>
+                                        </Table>
+                                    }
+                                />
+                            </Col>
+                        </Row>
+                    </Grid>
+                </div>
+            </div>
         </div>
     )
 }
