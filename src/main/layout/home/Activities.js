f0a572b3480b75ada1e7dcdb5409ed58fac1f488
Igboanugwo Collins <munisco12@gmail.com>
2020-11-05 00:26:32 +0100

    Working on the withdrawal and deposit histories

--- src/main/layout/home/Activities.js
+++ src/main/layout/home/Activities.js
@@ -17,17 +17,17 @@ const Activities = () => {
                 <div className='activity'>
                     <CgCalendarDates className='activity__icon' />
                     <h2 className='heading-4__light'>Started Date</h2>
-                    <h3 className='heading-4__dark'>july 5, 2019</h3>
+                    <h3 className='heading-4__dark'>july 5, 2010</h3>
                 </div>
                 <div className='activity'>
                     <RiBaseStationLine className='activity__icon' />
-                    <h2 className='heading-4__light'>Only days</h2>
-                    <h3 className='heading-4__dark'>482</h3>
+                    <h2 className='heading-4__light'>Online days</h2>
+                    <h3 className='heading-4__dark'>4005</h3>
                 </div>
                 <div className='activity'>
                     <BsPeopleFill className='activity__icon' />
                     <h2 className='heading-4__light'>Total members</h2>
-                    <h3 className='heading-4__dark'>3579</h3>
+                    <h3 className='heading-4__dark'>73,579,864</h3>
                 </div>
                 <div className='activity'>
                     <BsFillPersonCheckFill className='activity__icon' />
@@ -37,22 +37,22 @@ const Activities = () => {
                 <div className='activity'>
                     <BsFillPersonDashFill className='activity__icon' />
                     <h2 className='heading-4__light'>Total paid out</h2>
-                    <h3 className='heading-4__dark'>$12387915.79</h3>
+                    <h3 className='heading-4__dark'>$213,879,015+</h3>
                 </div>
                 <div className='activity'>
                     <BsFillPersonPlusFill className='activity__icon' />
                     <h2 className='heading-4__light'>Total Investments</h2>
-                    <h3 className='heading-4__dark'>$7540604.49</h3>
+                    <h3 className='heading-4__dark'>$355,406,804+</h3>
                 </div>
                 <div className='activity'>
                     <ImDownload className='activity__icon' />
                     <h2 className='heading-4__light'>Last Deposit</h2>
-                    <h3 className='heading-4__dark'>$57.00 (Francis1)</h3>
+                    <h3 className='heading-4__dark'>$57 (Francis1)</h3>
                 </div>
                 <div className='activity'>
                     <ImUpload className='activity__icon' />
                     <h2 className='heading-4__light'>Last withdrawal</h2>
-                    <h3 className='heading-4__dark'>$4821.00 (MICHEAL12)</h3>
+                    <h3 className='heading-4__dark'>$4821 (MICHEAL12)</h3>
                 </div>
             </div>
         </>
