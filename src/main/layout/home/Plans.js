450b9adc4bbce274fb02bfc507feda461fab24f6
Igboanugwo Collins <munisco12@gmail.com>
2020-11-05 14:53:03 +0100

    Working on the plans

--- src/main/layout/home/Plans.js
+++ src/main/layout/home/Plans.js
@@ -1,175 +1,123 @@
 import React from 'react'
 
 function Plans() {
-
     return (
         <>
-          <div className='plans__heading'>
+            <div className='plans__heading'>
                 <h1 className='plans__heading--text heading-1'>
                     Our Investment Plans
                 </h1>
             </div>
-        <div className='plans'>
-            
-           <div className='plan'>
-                        <div className='plan__icon'>
-                            <h1 className='heading-1 plan__icon-percent'>
-                                20%
-                            </h1>
-                            <h1 className='heading-1 plan__icon-text'>
-                                Plan 1
-                            </h1>
-                        </div>
-
-                        <ul className='plan__list'>
-                            <li className='plan__list-item'>
-                                AFTER <span className='plan__list-item--span'>1</span> DAY
-                            </li>
-                            <li className='plan__list-item'>
-                                minimum - <span className='plan__list-item--span'>$20</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                maximum - <span className='plan__list-item--span'>$499</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                Principal Return - <span className='plan__list-item--span'>YES</span>
-                            </li>
-                        </ul>
-                                          </div>
-                    <div className='plan'>
-                        <div className='plan__icon'>
-                            <h1 className='heading-1 plan__icon-percent'>
-                                30%
-                            </h1>
-                            <h1 className='heading-1 plan__icon-text'>
-                                Plan 2
-                            </h1>
-                        </div>
-
-                       <ul className='plan__list'>
-                            <li className='plan__list-item'>
-                                AFTER <span className='plan__list-item--span'>1</span> DAY
-                            </li>
-                            <li className='plan__list-item'>
-                                minimum - <span className='plan__list-item--span'>$500</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                maximum - <span className='plan__list-item--span'>$999</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                Principal Return - <span className='plan__list-item--span'>YES</span>
-                            </li>
-                        </ul>
-                        
+            <div className='plans'>
+                <div className='plan'>
+                    <div className='plan__icon'>
+                        <h1 className='heading-1 plan__icon-percent'>20%</h1>
+                        <h1 className='heading-1 plan__icon-text'>Plan 1</h1>
                     </div>
 
-                    <div className='plan'>
-                        <div className='plan__icon'>
-                            <h1 className='heading-1 plan__icon-percent'>
-                                50%
-                            </h1>
-                            <h1 className='heading-1 plan__icon-text'>
-                                Plan 3
-                            </h1>
-                        </div>
+                    <ul className='plan__list'>
+                        <li className='plan__list-item'>
+                            AFTER{' '}
+                            <span className='plan__list-item--span'>1</span> DAY
+                        </li>
+                        <li className='plan__list-item'>
+                            minimum -{' '}
+                            <span className='plan__list-item--span'>$20</span>
+                        </li>
+                        <li className='plan__list-item'>
+                            maximum -{' '}
+                            <span className='plan__list-item--span'>$499</span>
+                        </li>
+                        <li className='plan__list-item'>
+                            Principal Return -{' '}
+                            <span className='plan__list-item--span'>YES</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className='plan'>
+                    <div className='plan__icon'>
+                        <h1 className='heading-1 plan__icon-percent'>30%</h1>
+                        <h1 className='heading-1 plan__icon-text'>Plan 2</h1>
+                    </div>
 
-                       <ul className='plan__list'>
-                            <li className='plan__list-item'>
-                                AFTER <span className='plan__list-item--span'>2</span> DAYS
-                            </li>
-                            <li className='plan__list-item'>
-                                minimum - <span className='plan__list-item--span'>$1000</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                maximum - <span className='plan__list-item--span'>$1000000</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                Principal Return - <span className='plan__list-item--span'>YES</span>
-                            </li>
-                        </ul>
-                        
-                    </div>
-                    <div className='plan'>
-                        <div className='plan__icon'>
-                            <h1 className='heading-1 plan__icon-percent'>
-                                80%
-                            </h1>
-                            <h1 className='heading-1 plan__icon-text'>
-                                Plan 4
-                            </h1>
-                        </div>
+                    <ul className='plan__list'>
+                        <li className='plan__list-item'>
+                            AFTER{' '}
+                            <span className='plan__list-item--span'>1</span> DAY
+                        </li>
+                        <li className='plan__list-item'>
+                            minimum -{' '}
+                            <span className='plan__list-item--span'>$500</span>
+                        </li>
+                        <li className='plan__list-item'>
+                            maximum -{' '}
+                            <span className='plan__list-item--span'>$999</span>
+                        </li>
+                        <li className='plan__list-item'>
+                            Principal Return -{' '}
+                            <span className='plan__list-item--span'>YES</span>
+                        </li>
+                    </ul>
+                </div>
 
-                       <ul className='plan__list'>
-                            <li className='plan__list-item'>
-                                AFTER <span className='plan__list-item--span'>2</span> DAYS
-                            </li>
-                            <li className='plan__list-item'>
-                                minimum - <span className='plan__list-item--span'>$2000</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                maximum - <span className='plan__list-item--span'>$1000000</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                Principal Return - <span className='plan__list-item--span'>YES</span>
-                            </li>
-                        </ul>
-                        
+                <div className='plan'>
+                    <div className='plan__icon'>
+                        <h1 className='heading-1 plan__icon-percent'>50%</h1>
+                        <h1 className='heading-1 plan__icon-text'>Plan 3</h1>
                     </div>
-                    <div className='plan'>
-                        <div className='plan__icon'>
-                            <h1 className='heading-1 plan__icon-percent'>
-                                150%
-                            </h1>
-                            <h1 className='heading-1 plan__icon-text'>
-                                Gold
-                            </h1>
-                        </div>
 
-                       <ul className='plan__list'>
-                            <li className='plan__list-item'>
-                                AFTER <span className='plan__list-item--span'>3</span> DAYS
-                            </li>
-                            <li className='plan__list-item'>
-                                minimum - <span className='plan__list-item--span'>$4000</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                maximum - <span className='plan__list-item--span'>$1000000</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                Principal Return - <span className='plan__list-item--span'>YES</span>
-                            </li>
-                        </ul>
-                        
+                    <ul className='plan__list'>
+                        <li className='plan__list-item'>
+                            AFTER{' '}
+                            <span className='plan__list-item--span'>2</span>{' '}
+                            DAYS
+                        </li>
+                        <li className='plan__list-item'>
+                            minimum -{' '}
+                            <span className='plan__list-item--span'>$1000</span>
+                        </li>
+                        <li className='plan__list-item'>
+                            maximum -{' '}
+                            <span className='plan__list-item--span'>
+                                $1000000
+                            </span>
+                        </li>
+                        <li className='plan__list-item'>
+                            Principal Return -{' '}
+                            <span className='plan__list-item--span'>YES</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className='plan'>
+                    <div className='plan__icon'>
+                        <h1 className='heading-1 plan__icon-percent'>80%</h1>
+                        <h1 className='heading-1 plan__icon-text'>Plan 4</h1>
                     </div>
-                    <div className='plan'>
-                        <div className='plan__icon'>
-                            <h1 className='heading-1 plan__icon-percent'>
-                                200%
-                            </h1>
-                            <h1 className='heading-1 plan__icon-text'>
-                                Diamond
-                            </h1>
-                        </div>
 
-                       <ul className='plan__list'>
-                            <li className='plan__list-item'>
-                                AFTER <span className='plan__list-item--span'>4</span> DAYS
-                            </li>
-                            <li className='plan__list-item'>
-                                minimum - <span className='plan__list-item--span'>$6000</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                maximum - <span className='plan__list-item--span'>$1000000</span>
-                            </li>
-                            <li className='plan__list-item'>
-                                Principal Return - <span className='plan__list-item--span'>YES</span>
-                            </li>
-                        </ul>
-                        
-                    </div>
-                    </div>
-                    </>
-    
+                    <ul className='plan__list'>
+                        <li className='plan__list-item'>
+                            AFTER{' '}
+                            <span className='plan__list-item--span'>2</span>{' '}
+                            DAYS
+                        </li>
+                        <li className='plan__list-item'>
+                            minimum -{' '}
+                            <span className='plan__list-item--span'>$2000</span>
+                        </li>
+                        <li className='plan__list-item'>
+                            maximum -{' '}
+                            <span className='plan__list-item--span'>
+                                $1000000
+                            </span>
+                        </li>
+                        <li className='plan__list-item'>
+                            Principal Return -{' '}
+                            <span className='plan__list-item--span'>YES</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
     )
 }
 
