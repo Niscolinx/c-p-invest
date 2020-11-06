3488753673fe27672fc6da8316cb0d02fbddb9dd
Igboanugwo Collins <munisco12@gmail.com>
2020-11-06 23:25:53 +0100

    working on the transactions

--- src/main/layout/home/Transactions.js
+++ src/main/layout/home/Transactions.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
-import EthereumGif from '../../../images/ethereum.png'
+//import EthereumGif from '../../../images/ethereum.png'
 import BitcoinGif from '../../../images/bitcoin-gif.gif'
 
 function Transactions(props) {
@@ -12,14 +12,13 @@ function Transactions(props) {
             setLatestDeposits(props.latestDeposits)
         }
 
-        if(props.latestWithdrawals){
+        if (props.latestWithdrawals) {
             setLatestWithdrawals(props.latestWithdrawals)
         }
     }, [props])
 
-    console.log('the transaction', latestDeposits, latestWithdrawals)
 
-    const theLatestDeposits = latestDeposits.map((key, d) => {
+    const theLatestDeposits = latestDeposits.map((d, key) => {
         return (
             <div className='transactions__col' key={key}>
                 <img
@@ -30,216 +29,132 @@ function Transactions(props) {
                 <p className='transactions__amount'>${d.amount}</p>
 
                 <p className='transactions__date'>{d.updatedAt}</p>
-        <h4 className='transactions__name'>{d.username}</h4>
+                <h4 className='transactions__name'>{d.creator}</h4>
+            </div>
+        )
+    })
+    const theLatestWithdrawals = latestWithdrawals.map((d, key) => {
+        return (
+            <div className='transactions__col' key={key}>
+                <img
+                    src={BitcoinGif}
+                    alt=''
+                    className='transactions__currency'
+                />
+                <p className='transactions__amount'>${d.amount}</p>
+
+                <p className='transactions__date'>{d.updatedAt}</p>
+                <h4 className='transactions__name'>{d.creator}</h4>
             </div>
         )
     })
 
+    // let toRender = (
+    //     <>
+    //         <div className='transactions__col'>
+    //             <img
+    //                 src={BitcoinGif}
+    //                 alt=''
+    //                 className='transactions__currency'
+    //             />
+    //             <p className='transactions__amount'>$57.00</p>
 
-    let toRender = (  <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$57.00</p>
+    //             <p className='transactions__date'>Oct-28-2020</p>
+    //             <h4 className='transactions__name'>Francis1</h4>
+    //         </div>
+    //         <div className='transactions__col'>
+    //             <img
+    //                 src={BitcoinGif}
+    //                 alt=''
+    //                 className='transactions__currency'
+    //             />
+    //             <p className='transactions__amount'>$853.00</p>
 
-                    <p className='transactions__date'>Oct-28-2020</p>
-                    <h4 className='transactions__name'>Francis1</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$853.00</p>
+    //             <p className='transactions__date'>Oct-28-2020</p>
+    //             <h4 className='transactions__name'>Peter107</h4>
+    //         </div>
+    //         <div className='transactions__col'>
+    //             <img
+    //                 src={BitcoinGif}
+    //                 alt=''
+    //                 className='transactions__currency'
+    //             />
+    //             <p className='transactions__amount'>$902.00</p>
 
-                    <p className='transactions__date'>Oct-28-2020</p>
-                    <h4 className='transactions__name'>Peter107</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$902.00</p>
+    //             <p className='transactions__date'>Oct-28-2020</p>
+    //             <h4 className='transactions__name'>Reginald89</h4>
+    //         </div>
+    //         <div className='transactions__col'>
+    //             <img
+    //                 src={BitcoinGif}
+    //                 alt=''
+    //                 className='transactions__currency'
+    //             />
+    //             <p className='transactions__amount'>$5392.00</p>
 
-                    <p className='transactions__date'>Oct-28-2020</p>
-                    <h4 className='transactions__name'>Reginald89</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$5392.00</p>
+    //             <p className='transactions__date'>Oct-28-2020</p>
+    //             <h4 className='transactions__name'>Angelia</h4>
+    //         </div>
+    //         <div className='transactions__col'>
+    //             <img
+    //                 src={BitcoinGif}
+    //                 alt=''
+    //                 className='transactions__currency'
+    //             />
+    //             <p className='transactions__amount'>$892.00</p>
 
-                    <p className='transactions__date'>Oct-28-2020</p>
-                    <h4 className='transactions__name'>Angelia</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$892.00</p>
+    //             <p className='transactions__date'>Oct-27-2020</p>
+    //             <h4 className='transactions__name'>Bahk103</h4>
+    //         </div>
+    //         <div className='transactions__col'>
+    //             <img
+    //                 src={BitcoinGif}
+    //                 alt=''
+    //                 className='transactions__currency'
+    //             />
+    //             <p className='transactions__amount'>$719.00</p>
 
-                    <p className='transactions__date'>Oct-27-2020</p>
-                    <h4 className='transactions__name'>Bahk103</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$719.00</p>
+    //             <p className='transactions__date'>Oct-27-2020</p>
+    //             <h4 className='transactions__name'>Stephen01</h4>
+    //         </div>
+    //         <div className='transactions__col'>
+    //             <img
+    //                 src={BitcoinGif}
+    //                 alt=''
+    //                 className='transactions__currency'
+    //             />
+    //             <p className='transactions__amount'>$3092.00</p>
 
-                    <p className='transactions__date'>Oct-27-2020</p>
-                    <h4 className='transactions__name'>Stephen01</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$3092.00</p>
+    //             <p className='transactions__date'>Oct-27-2020</p>
+    //             <h4 className='transactions__name'>Lee</h4>
+    //         </div>
+    //         <div className='transactions__col'>
+    //             <img
+    //                 src={BitcoinGif}
+    //                 alt=''
+    //                 className='transactions__currency'
+    //             />
+    //             <p className='transactions__amount'>$112.00</p>
 
-                    <p className='transactions__date'>Oct-27-2020</p>
-                    <h4 className='transactions__name'>Lee</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$112.00</p>
+    //             <p className='transactions__date'>Oct-26-2020</p>
+    //             <h4 className='transactions__name'>AslanLion</h4>
+    //         </div>
+    //     </>
+    // )
 
-                    <p className='transactions__date'>Oct-26-2020</p>
-                    <h4 className='transactions__name'>AslanLion</h4>
-                </div>)
-
-    if(latestDeposits){
-
-        toRender = theLatestDeposits
-    }
     return (
         <>
             <div className='transactions'>
                 <h4 className='heading-4 transactions__heading mb-sm'>
                     Latest Deposits
                 </h4>
-              
+                {theLatestDeposits}
             </div>
             <div className='transactions'>
                 <h4 className='heading-4 transactions__heading mb-sm'>
                     Latest Withdrawals
                 </h4>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$4821.00</p>
-
-                    <p className='transactions__date'>Oct-28-2020</p>
-                    <h4 className='transactions__name'>MICHEAL12</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$780.00</p>
-
-                    <p className='transactions__date'>Oct-28-2020</p>
-                    <h4 className='transactions__name'>Henry2</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$1231.00</p>
-
-                    <p className='transactions__date'>Oct-28-2020</p>
-                    <h4 className='transactions__name'>Lion001</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$630.00</p>
-
-                    <p className='transactions__date'>Oct-27-2020</p>
-                    <h4 className='transactions__name'>Fabio</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$28.00</p>
-
-                    <p className='transactions__date'>Oct-27-2020</p>
-                    <h4 className='transactions__name'>Joan</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$4651.00</p>
-
-                    <p className='transactions__date'>Oct-27-2020</p>
-                    <h4 className='transactions__name'>Loveline112</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$117.00</p>
-
-                    <p className='transactions__date'>Oct-27-2020</p>
-                    <h4 className='transactions__name'>Lugard32</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$49.00</p>
-
-                    <p className='transactions__date'>Oct-26-2020</p>
-                    <h4 className='transactions__name'>Timothy</h4>
-                </div>
-                <div className='transactions__col'>
-                    <img
-                        src={BitcoinGif}
-                        alt=''
-                        className='transactions__currency'
-                    />
-                    <p className='transactions__amount'>$312.00</p>
-
-                    <p className='transactions__date'>Oct-26-2020</p>
-                    <h4 className='transactions__name'>Elizabeth</h4>
-                </div>
+               {theLatestWithdrawals}
             </div>
         </>
     )
